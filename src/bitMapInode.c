5e3df15b908c6626c3b3e0a7ca3783b7f45e2431
Pamela <pamelalarad05@gmail.com>
2026-06-13 10:10:45 -0300

    feat: estrutura inicial inode

---
+++ src/bitMapInode.c
@@ -0,0 +1,77 @@
+#include "bitMapInode.h"
+
+#include <stdio.h>
+
+
+
+void inicializarBitmapInodes(bitmapInode *bitmap) {
+    
+    for (int i = 0; i < NUM_INODES; i++) {
+        bitmap->bitmap[i] = 0;
+    }
+}
+
+
+int marcarInodeOcupado(bitmapInode* bitmap, int idInode) {
+    
+    if (!idInodeValido(idInode)) {
+        return -1;
+    }
+
+    bitmap->bitmap[idInode] = 1;
+    return 0;
+}
+
+int marcarInodeLivre(bitmapInode *bitmap, int idInode) {
+    
+    if (!idInodeValido(idInode)) {
+        return -1;
+    }
+
+    bitmap->bitmap[idInode] = 0;
+    return 0;
+}
+
+int buscarInodeLivre(bitmapInode *bitmap) {
+    
+    for (int i = 0; i < NUM_INODES; i++) {
+        if (bitmap->bitmap[i] == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int alocarInode(bitmapInode *bitmap, iNode tabelaInodes[]) {
+    
+    int idLivre = buscarInodeLivre(bitmap);
+
+    if (idLivre == -1) {
+        return -1;
+    }
+
+    marcarInodeOcupado(bitmap, idLivre);
+
+    preencherInode(&tabelaInodes[idLivre]);
+    tabelaInodes[idLivre].idInode = idLivre;
+
+    return idLivre;
+}
+
+int desalocarInode(bitmapInode *bitmap, iNode tabelaInodes[], int idInode) {
+    
+    if (!idInodeValido(idInode)) {
+        return -1;
+    }
+
+    if (bitmap->bitmap[idInode] == 0) {
+        return -1;
+    }
+
+    marcarInodeLivre(bitmap, idInode);
+
+    liberarInode(&tabelaInodes[idInode]);
+    tabelaInodes[idInode].idInode = idInode;
+
+    return 0;
+}
