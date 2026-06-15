c9ae5ac09335cf6d4b0aa36b1bee1803dde50ed5
helom <helomf.marques@gmail.com>
2026-06-15 16:41:39 -0300

    a

--- include/interpretador.h
+++ include/interpretador.h
@@ -1,7 +1,6 @@
 #ifndef INTERPRETADOR_H
 #define INTERPRETADOR_H
 
-// Inclui os cabeçalhos das tuas estruturas para que o interpretador conheça os tipos de dados
 #include "Diretorio.h"
 #include "superbloco.h"
 #include "bitMapInode.h"
